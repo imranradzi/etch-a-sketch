9477978588d9159df4f7707e1ef340048bbfe4cb
Imran <imranradziii@gmail.com>
2022-06-22 20:42:42 +0100

    added styles.css to style each of the grids accordingly, modified script.js so that each grid turns black when mouse is hovered over it, and goes white when mouse is no longer hovering over it

--- script.js
+++ script.js
@@ -7,21 +7,31 @@ container.setAttribute('class', 'container');
 container.textContent = 'this will contain our square grids';
 
 body.appendChild(container);
-container.setAttribute('style', 'display: flex; flex-direction: column');
+container.setAttribute('style', 'display: flex; flex-direction: column; width: 960px');
 
 // create 16 rows and 16 columns using nested for loops
 
 for (let i = 0; i < 16; i++) {
   let row = document.createElement('div');
   container.appendChild(row);
-  row.setAttribute('style', `display: flex; flex-direction: row; flex-basis: 50px;
-                      flex-grow: 1`);
+  row.setAttribute('class', 'row');
 
   for (let j = 0; j < 16; j++) {
-    let column = document.createElement('div');
-    column.textContent = `c${j + 1}`;
-    row.appendChild(column);
-    column.setAttribute('class', 'grid-block');
-    column.setAttribute('style', 'flex-basis: 50px; flex-grow: 1');
+    let grid = document.createElement('div');
+    grid.textContent = `${(j + 1) + 16 * (i - 0)}`;
+    row.appendChild(grid);
+    grid.setAttribute('class', 'grid-block');
+
+    // changes grid colour when mouse hovers
+    grid.addEventListener("mouseenter", function(e) {
+      e.target.setAttribute('style', 'background-color: black;')
+    }
+    );
+
+    grid.addEventListener("mouseout", function(e) {
+      e.target.setAttribute('style', 'background-color: transparent; transition: opacity 1s')
+    }
+    );
+
   }
 }
