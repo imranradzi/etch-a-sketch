3571b2ee4da7b893ad4dc90bb98177f92751a993
Imran <imranradziii@gmail.com>
2022-06-22 21:21:39 +0100

    added a button which, when pressed, will prompt the user for a number which we call dimension, and which will create a new grid with size dimension x dimension

--- script.js
+++ script.js
@@ -7,27 +7,47 @@ container.setAttribute('class', 'container');
 
 body.appendChild(container);
 
-// create 16 rows and 16 columns using nested for loops
+// create n rows and n columns using nested for loops
 
-let n = 32;
-for (let i = 0; i < n; i++) {
-  let row = document.createElement('div');
-  container.appendChild(row);
-  row.setAttribute('class', 'row');
+function createGrid(n) {
+  // create n row
+  for (let i = 0; i < n; i++) {
+    let row = document.createElement('div');
+    container.appendChild(row);
+    row.setAttribute('class', 'row');
 
-  for (let j = 0; j < n; j++) {
-    let grid = document.createElement('div');
-    row.appendChild(grid);
-    grid.setAttribute('class', 'grid-block');
+    // for each row, we will have n grids
+    for (let j = 0; j < n; j++) {
+      let grid = document.createElement('div');
+      row.appendChild(grid);
+      grid.setAttribute('class', 'grid-block');
 
-    // changes grid colour when mouse hovers
-    grid.addEventListener("mouseenter", function(e) {
-      e.target.setAttribute('style', 'background-color: black;');
+      // changes grid colour when mouse hovers
+      grid.addEventListener("mouseenter", function(e) {
+        e.target.setAttribute('style', 'background-color: black;');
 
-      /* reset color after some delay
-      setTimeout(function() {
-        e.target.setAttribute('style', 'background-color: transparent;');
-      }, 500);*/
-    });
+        /* reset color after some delay
+        setTimeout(function() {
+          e.target.setAttribute('style', 'background-color: transparent;');
+        }, 500);*/
+      });
+    }
   }
 }
+
+let dimension = 0;
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+  dimension = prompt('Enter a value between 0 and 100!');
+  if (dimension => 0 && dimension <= 100) {
+    container.replaceChildren();
+    createGrid(dimension);
+  }
+});
+
+
+// create 16 x 16 grids
+let n = 16;
+createGrid(n);
+
+
