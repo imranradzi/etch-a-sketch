3a44fc369045bb396c387b96d5b107e8d8e132ca
Imran <imranradziii@gmail.com>
2022-06-22 20:59:38 +0100

    changed script.js to change colour of grids permanently when mouse goes over a grid, and fixed the dimension on which etch-a-sketch works

--- script.js
+++ script.js
@@ -11,27 +11,25 @@ container.setAttribute('style', 'display: flex; flex-direction: column; width: 9
 
 // create 16 rows and 16 columns using nested for loops
 
-for (let i = 0; i < 16; i++) {
+let n = 32;
+for (let i = 0; i < n; i++) {
   let row = document.createElement('div');
   container.appendChild(row);
   row.setAttribute('class', 'row');
 
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < n; j++) {
     let grid = document.createElement('div');
-    grid.textContent = `${(j + 1) + 16 * (i - 0)}`;
     row.appendChild(grid);
     grid.setAttribute('class', 'grid-block');
 
     // changes grid colour when mouse hovers
     grid.addEventListener("mouseenter", function(e) {
-      e.target.setAttribute('style', 'background-color: black;')
-    }
-    );
+      e.target.setAttribute('style', 'background-color: black;');
 
-    grid.addEventListener("mouseout", function(e) {
-      e.target.setAttribute('style', 'background-color: transparent; transition: opacity 1s')
-    }
-    );
-
+      /* reset color after some delay
+      setTimeout(function() {
+        e.target.setAttribute('style', 'background-color: transparent;');
+      }, 500);*/
+    });
   }
 }
